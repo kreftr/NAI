23a520bf8ddd8b30f429f05600b503481f15c812
s18868 <rafalkreft02@gmail.com>
2020-10-13 17:57:33 +0200

    initial commit

---
+++ lab1/example.cpp
@@ -0,0 +1,9 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui_c.h>
+#include <iostream>
+int main( int argc, char** argv )
+{
+ cv::namedWindow("Okienko Prawie Inteligentne", 1);
+ cv::waitKey(0);
+ return 0;
+}
